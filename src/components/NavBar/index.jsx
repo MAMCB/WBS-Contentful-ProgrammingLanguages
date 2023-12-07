c46bc3f1bdf10dcac8abe72cfa89f8601c0f07da
SabinaMB <sabina.marian.blaariu@gmail.com>
2023-12-07 22:48:26 +0100

    navbar styled

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,24 +1,28 @@
-
 import React from 'react';
-import { Navbar, Container, Nav } from 'react-bootstrap';
-import './index.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
-const NavBar = () => {
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css'; 
+function BasicExample() {
   return (
-    <Navbar bg="dark" variant="dark" expand="lg">
-      <Container>
-        <Navbar.Brand href="#">Home</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbar" />
-        <Navbar.Collapse id="navbar" className="justify-content-end">
-          <Nav>
-            <Nav.Link href="#">Programming Languages</Nav.Link>
-            <Nav.Link href="#">Scripting Languages</Nav.Link>
-            <Nav.Link href="#">Querying Languages</Nav.Link>
+    <Navbar expand="lg" className="bg-body-tertiary fixed-top">
+      <Container fluid> {/*to occupy the entire width*/}
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Programming languages</Nav.Link>
+            <Nav.Link href="#link">Scripting languages</Nav.Link>
+            <Nav.Link href="#link">Query languages</Nav.Link>
+           
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
-};
+}
 
-export default NavBar;
+export default BasicExample;
