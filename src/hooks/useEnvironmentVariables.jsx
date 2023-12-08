bb4a2bf20a1aa68d6e68e52738c09003cecc728d
borabey <bora.b.sarikaya@gmail.com>
2023-12-08 10:17:34 +0100

    bind contenful_cdn_url correctly to variables state

--- src/hooks/useEnvironmentVariables.jsx
+++ src/hooks/useEnvironmentVariables.jsx
@@ -1,11 +1,12 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 export const useEnvironmentVariables = () => {
-    const [variables, setVariables] = useState({
-        SPACE_ID: import.meta.env.VITE_SPACE_ID,
-        ENVIRONMENT_NAME: import.meta.env.VITE_ENVIRONMENT_NAME,
-        ACCESS_TOKEN: import.meta.env.VITE_ACCESS_TOKEN
-    });
+  const [variables, setVariables] = useState({
+    SPACE_ID: import.meta.env.VITE_SPACE_ID,
+    ENVIRONMENT_NAME: import.meta.env.VITE_ENVIRONMENT_NAME,
+    ACCESS_TOKEN: import.meta.env.VITE_ACCESS_TOKEN,
+    CONTENTFUL_CDN_URL: import.meta.env.VITE_CONTENTFUL_CDN_URL,
+  });
 
-    return variables;
+  return variables;
 };
