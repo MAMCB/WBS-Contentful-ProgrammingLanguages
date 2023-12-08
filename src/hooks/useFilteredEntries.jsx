85819e3d7e7172cb8d3d5352e6373fa298cfdac2
borabey <bora.b.sarikaya@gmail.com>
2023-12-08 17:22:22 +0100

    add customHook to filter by Entries

---
+++ src/hooks/useFilteredEntries.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import { createClient } from "contentful";
+import { useEnvironmentVariables } from "./useEnvironmentVariables";
+
+export const useFilteredEntries = ({
+  searchQuery,
+  fieldToFilter,
+  content_type,
+}) => {
+  const { SPACE_ID, ACCESS_TOKEN } = useEnvironmentVariables();
+  const [entries, setEntries] = useState([]);
+
+  useEffect(() => {
+    const client = createClient({
+      space: SPACE_ID,
+      accessToken: ACCESS_TOKEN,
+    });
+
+    client
+      .getEntries({
+        content_type: content_type,
+        [`fields.${fieldToFilter}`]: searchQuery,
+      })
+      .then((entries) => {
+        if (entries.items) {
+          setEntries(entries.items);
+        }
+      })
+      .catch((error) => {
+        console.log("Error fetching entries", error);
+      });
+  }, []);
+
+  return entries;
+};
