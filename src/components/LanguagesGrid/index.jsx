615819330becd46875bf6b4d00d6afe39c2f4c4f
borabey <bora.b.sarikaya@gmail.com>
2023-12-11 13:48:35 +0100

    map over entries and populate itemcards

--- src/components/LanguagesGrid/index.jsx
+++ src/components/LanguagesGrid/index.jsx
@@ -1,8 +1,14 @@
+import ItemCard from "../ItemCard";
+
 const LanguagesGrid = ({entries}) => {
     console.log(entries, 'Grid');
     
     return (
         <>
+        {entries && entries.map((item ) => {
+           return  <ItemCard key={item.sys.id} fields={item.fields} /> 
+        })}
+        
         
         </>
     )
