abb81d9ea69fd8a67a69568c0a2c25d3b4774190
borabey <bora.b.sarikaya@gmail.com>
2023-12-11 16:47:19 +0100

    pass data to detail page based on params

--- src/pages/DetailPage/index.jsx
+++ src/pages/DetailPage/index.jsx
@@ -1,14 +1,34 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { useEntryById } from "../../hooks/useEntryById"
+import { useEntryById } from "../../hooks/useEntryById";
 
 const DetailPage = () => {
-    const { id } = useParams();
-    const [entry, setEntry] = useState(null);
+  const { id } = useParams();
+  const [entry, setEntry] = useState(null);
+  const entryData = useEntryById(id);
 
-    console.log(id);
-    const entryData = useEntryById(id);
+  useEffect(() => {
+    if (entryData) {
+      setEntry(entryData);
+    }
+  }, [entryData]);
 
+  if (!entry) {
+    return <div>Loading...</div>; // Display a loading state while fetching data
+  }
+  
+  if (!entry.fields) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <>
+      <h2>{entry.fields.name}</h2>
+      <h3>{entry.fields.type}</h3>
+      <p>{entry.fields.description}</p>
+      <p>{entry.fields.useCase}</p>
+    </>
+  );
 };
 
 export default DetailPage;
