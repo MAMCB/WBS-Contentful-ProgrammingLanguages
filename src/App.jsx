9196d85ce60c0bb28ba487fae8793feba0cbdc62
borabey <bora.b.sarikaya@gmail.com>
2023-12-11 14:05:07 +0100

    add new routes for not found and detailpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import { useState } from "react";
 import "./App.css";
 import { createClient } from "contentful";
-import { Routes,Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/HomePage/Home";
-import { useEnvironmentVariables } from './hooks/useEnvironmentVariables';
-import LanguagesPage from './pages/LanguagesPage'
-
+import { useEnvironmentVariables } from "./hooks/useEnvironmentVariables";
+import LanguagesPage from "./pages/LanguagesPage";
+import DetailPage from "./pages/DetailPage";
+import NotFound from "./pages/NotFound";
 
 function App() {
-  const[languages,setLanguages]=useState([])
-  const { SPACE_ID, ENVIRONMENT_NAME, ACCESS_TOKEN } = useEnvironmentVariables();
+  const [languages, setLanguages] = useState([]);
+  const { SPACE_ID, ENVIRONMENT_NAME, ACCESS_TOKEN } =
+    useEnvironmentVariables();
 
   const client = createClient({
     space: SPACE_ID,
     environment: ENVIRONMENT_NAME,
-    accessToken: ACCESS_TOKEN
+    accessToken: ACCESS_TOKEN,
   });
 
   return (
@@ -23,28 +25,12 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route
           path="/programming"
-          element={
-            <LanguagesPage
-              type="Programming"
-            />
-          }
+          element={<LanguagesPage type="Programming" />}
         />
-        <Route
-          path="/script"
-          element={
-            <LanguagesPage
-              type="Scripting"
-            />
-          }
-        />
-        <Route
-          path="/query"
-          element={
-            <LanguagesPage
-              type="Query"
-            />
-          }
-        />
+        <Route path="/script" element={<LanguagesPage type="Scripting" />} />
+        <Route path="/query" element={<LanguagesPage type="Query" />} />
+        <Route path="/products/:id" element={<DetailPage />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </>
   );
