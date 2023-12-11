77c593500e2344419181a1e7c8eaf179010781e5
borabey <bora.b.sarikaya@gmail.com>
2023-12-11 15:48:44 +0100

    create hook to get entry by id

---
+++ src/hooks/useEntryById.jsx
@@ -0,0 +1,22 @@
+import { createClient } from "contentful";
+import { useEnvironmentVariables } from "./useEnvironmentVariables";
+import { useState, useEffect } from 'react';
+
+export const useEntryById = ( entryId) => { 
+  const { SPACE_ID, ACCESS_TOKEN } = useEnvironmentVariables();
+  const [entry, setEntry] = useState({});
+
+  useEffect(() => {
+    const client = createClient({
+      space: SPACE_ID,
+      accessToken: ACCESS_TOKEN,
+    });
+
+    client
+      .getEntry(entryId)
+      .then((entry) => setEntry(entry))
+      .catch((error) => error.log(error));
+  }, []);
+
+  return entry;
+};
